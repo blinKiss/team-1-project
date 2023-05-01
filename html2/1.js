433e910601d439f9c518dd932587f9bc54027120
blinKiss <blinkiss@naver.com>
2023-05-02 00:34:32 +0900

    2

--- html2/1.js
+++
@@ -1,7 +0,0 @@
-// JavaScript 코드
-const button = document.querySelector('.toggle-menu-button');
-const menuList = document.querySelector('.toggle-menu-list');
-
-button.addEventListener('click', () => {
-  menuList.classList.toggle('show');
-});
