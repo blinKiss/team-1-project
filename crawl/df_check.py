b6b9debccaea9d90b446fe14b3af8de306e763a4
blinKiss <blinkiss@naver.com>
2023-05-01 13:31:37 +0900

    01

--- crawl/df_check.py
+++ crawl/df_check.py
@@ -12,9 +12,21 @@ import pandas as pd
 # df = pd.read_csv('./team-1-project/data/popular_songs.csv')
 # print(df[df.isnull().any(axis=1)])
 
-df = pd.read_csv('./team-1-project/data/sympathy/여성_세대별_음악순위_네이바.csv')
-df2 = pd.read_csv('./team-1-project/data/sympathy/여성_세대별_음악순위_추가.csv')
+# df = pd.read_csv('./team-1-project/data/sympathy/여성_세대별_음악순위_네이바.csv')
+# df2 = pd.read_csv('./team-1-project/data/sympathy/여성_세대별_음악순위_추가.csv')
 
-df3 = pd.concat([df, df2], ignore_index=True).drop_duplicates()
+# df3 = pd.concat([df, df2], ignore_index=True).drop_duplicates()
 # print(df3)
-df3.to_csv('./team-1-project/data/sympathy/여성_세대별_음악순위.csv', index=False)
+# df3.to_csv('./team-1-project/data/sympathy/여성_세대별_음악순위.csv', index=False)
+
+
+df = pd.read_csv('./team-1-project/data/sympathy/남성_세대별_음악순위.csv')
+# df['앨범이미지'] = df['앨범이미지'].str.replace('%3Ftype.*', '')
+# df = df.drop(columns='Unnamed: 0')
+
+df['앨범이미지'] = df['앨범이미지'].apply(lambda x: x + '?type=r480Fll')
+# print(df['앨범이미지'])
+# print(df)
+df.to_csv('./team-1-project/data/sympathy/남성_세대별_음악순위.csv', index=False)
+            # j = '.jpg'
+            # album_img = album_img_temp[:album_img_temp.index(j)+len(j)]
